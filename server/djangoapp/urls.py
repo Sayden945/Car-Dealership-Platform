cfbe87e8339eb0868a0c7d83f6fff1d61190e1ae
Sayden945 <s_ayden@ymail.com>
2024-05-07 11:51:15 +1000

    Fixed manifest.json issue

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path for registration
     path(route="register", view=views.registration, name="register"),
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    path(route="login", view=views.login_user, name="login"),
     # path for dealer reviews view
     path(route="logout", view=views.logout_request, name="logout"),
     # path for add a review view
